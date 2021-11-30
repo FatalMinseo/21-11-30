4247e10db0c430b6e029e5921949ca36c2cd09c8
FatalMinseo <95062577+FatalMinseo@users.noreply.github.com>
2021-11-30 13:31:49 +0900

    Add files via upload

---
+++ Exam02.java
@@ -0,0 +1,19 @@
+
+public class Exam02 {
+
+	public static void main(String[] args) {
+		 
+		// 여러가지 변수의 특징
+		int num = 20;
+		// 같은 이름의 변수를 생성할 수 없다
+		// int num = 30;
+		num = 50;
+		// 변수에 값을 담는 순간 이전의 값은 사라지게 된다
+		System.out.println(num);
+		// int age = 20;
+		int age;
+		age = 20;
+
+	}
+
+}
